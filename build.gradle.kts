820c78bef62c56e738ab0bca76eb5d3749aeeafc
Ravikant108 <ravikantkrsingh108@gmail.com>
2026-07-08 11:47:26 +0530

    Update repository URL in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,7 +53,7 @@ subprojects {
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        setRepo("https://github.com/NivinCNC/CNCVerse-Cloud-Stream-Extension")
+        setRepo("https://github.com/ravikantkrsingh108-dot/RvVerse")
         authors = listOf("NivinCNC")
     }
 
