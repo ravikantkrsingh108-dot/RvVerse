389ab15305a62102af4893280aa26ff8834fc976
Ravikant108 <ravikantkrsingh108@gmail.com>
2026-07-08 12:41:39 +0530

    Delete LivXowProvider directory

--- LivXowProvider/src/main/kotlin/com/cncverse/LivXowFirebaseFetcher.kt
+++
@@ -1,5 +0,0 @@
-package com.cncverse.livxow // Replace with the actual package name if different
-
-class LivXowFirebaseFetcher {
-    // Placeholder to bypass compilation errors
-}
--- LivXowProvider/src/main/kotlin/com/cncverse/LivXowLiveEventsProvider.kt
+++
@@ -1,5 +0,0 @@
-package com.cncverse.livxow // Replace with the actual package name if different
-
-class LivXowLiveEventsProvider {
-    // Placeholder to bypass compilation errors
-}
